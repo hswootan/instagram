6c08e082850292c418d277120c003e1c45f174fc
hong <hongwootan@outlook.com>
2021-10-08 17:16:12 -0400

    user can log out

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -67,6 +67,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
 }
     
     
@@ -101,5 +113,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
+
 
 
--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -24,6 +24,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
             window?.rootViewController = feedNavigationController
         }
+        
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
