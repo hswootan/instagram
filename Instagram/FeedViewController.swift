71a142b1992a06b1c9b35b6726d2f04c16e6f3c6
hong <hongwootan@outlook.com>
2021-10-11 21:40:55 -0400

    user can view comments on a post

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -12,27 +12,68 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let post = posts[indexPath.row]
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            cell.captionLabel.text = post["caption"] as! String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af_setImage(withURL: url)
+            
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row-1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.section]
         
-        cell.captionLabel.text = post["caption"] as! String
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a comment"
+        comment["popst"] = post
+        comment["author"] = PFUser.current()!
         
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
+        post.add(comment, forKey: "comments")
         
-        cell.photoView.af_setImage(withURL: url)
-        
-        
-        return cell
+        post.saveInBackground {(success, error) in
+            if success{
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
     }
     
 
@@ -54,7 +95,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground {(posts, error) in
@@ -80,28 +121,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 }
-    
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return posts.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
-//            as! PostCell
-//
-//        let post = posts[indexPath.row]
-//
-//        let user = post["author"] as! PFUser
-//        cell.usernameLabel.text = user.username
-//
-//        cell.captionLabel.text = post["caption"] as! String
-//
-//
-//        return cell
-//    }
-//
     
 
     /*
